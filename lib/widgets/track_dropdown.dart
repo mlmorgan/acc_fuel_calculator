32b1daf95e7e5a35aa64d351df12493c3ee463fb
mlmorgan <mlmorgan4859@gmail.com>
2021-04-26 20:31:26 +0100

    Add alternate flag designs

--- lib/widgets/track_dropdown.dart
+++ lib/widgets/track_dropdown.dart
@@ -1,7 +1,7 @@
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
-import 'package:flag/flag.dart';
 
 import '../providers/tracks.dart';
 import '../models/track/track.dart';
@@ -13,42 +13,46 @@ class TrackDropdown extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final shortestSide = MediaQuery.of(context).size.shortestSide;
+    final bool useMobileLayout = shortestSide < 600;
+
     return Consumer<Tracks>(
       builder: (ctx, tracks, _) => DropdownSearch<Track>(
-          label: 'Track',
-          mode: Mode.BOTTOM_SHEET,
-          showSearchBox: true,
-          searchDelay: Duration(milliseconds: 0),
-          dropdownBuilderSupportsNullItem: true,
-          items: tracks.tracks,
-          autoFocusSearchBox: true,
-          dropdownBuilder: (context, Track? track, designation) {
-            return track != null
-                ? TrackDropdownMenuItem(
+        label: 'Track',
+        mode: useMobileLayout ? Mode.BOTTOM_SHEET : Mode.DIALOG,
+        showSearchBox: true,
+        searchDelay: Duration(milliseconds: 0),
+        dropdownBuilderSupportsNullItem: true,
+        items: tracks.tracks,
+        autoFocusSearchBox: true,
+        dropdownBuilder: (context, Track? track, designation) {
+          return track != null
+              ? TrackDropdownMenuItem(
+                  track: track,
+                )
+              : SizedBox();
+        },
+        popupItemBuilder: (context, Track? track, isSelected) {
+          return track != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TrackDropdownMenuItem(
                     track: track,
-                  )
-                : SizedBox();
-          },
-          popupItemBuilder: (context, Track? track, isSelected) {
-            return track != null
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: TrackDropdownMenuItem(
-                      track: track,
-                    ),
-                  )
-                : SizedBox();
-          },
-          itemAsString: (Track? track) {
-            if (track != null) return track.name;
-            return '';
-          },
-          onChanged: (Track? newTrack) {
-            if (newTrack != null) {
-              tracks.setCurrentTrack(newTrack);
-            }
-            FocusScope.of(context).requestFocus(nextFocusNode);
-          }),
+                  ),
+                )
+              : SizedBox();
+        },
+        itemAsString: (Track? track) {
+          if (track != null) return track.name;
+          return '';
+        },
+        onChanged: (Track? newTrack) {
+          if (newTrack != null) {
+            tracks.setCurrentTrack(newTrack);
+          }
+          FocusScope.of(context).requestFocus(nextFocusNode);
+        },
+      ),
     );
   }
 }
@@ -72,16 +76,17 @@ class TrackDropdownMenuItem extends StatelessWidget {
             width: 20,
           ),
           Container(
-            child: Flag(
-              track.country.value,
-              height: 20,
-              width: 30,
-              fit: BoxFit.fill,
+            child: SvgPicture.asset(
+              'images/flags/${track.country.value}.svg',
+              width: 28,
             ),
             decoration: BoxDecoration(
               boxShadow: [
-                BoxShadow(),
+                BoxShadow(
+                  blurRadius: 1,
+                ),
               ],
+              shape: BoxShape.circle,
             ),
           ),
           SizedBox(
