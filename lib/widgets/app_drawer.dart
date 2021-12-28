3aa306de51f4a2107e64d006f6c630061985c097
Matthew Morgan <matthewm@apadmi.com>
2021-12-28 18:33:39 +0000

    Bump version to 1.2.1

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -64,7 +64,7 @@ class AppDrawer extends StatelessWidget {
               'images/icon/ic_launcher.png',
               width: 72,
             ),
-            applicationVersion: '1.2.0',
+            applicationVersion: '1.2.1',
             aboutBoxChildren: [
               InfoDialog(),
             ],
