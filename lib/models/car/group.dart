09e51b136f1478cab13bb50477c8ed7f3154169a
Matthew Morgan <matthewm@apadmi.com>
2022-07-19 18:37:47 +0100

    Add class indicator widget

--- lib/models/car/group.dart
+++ lib/models/car/group.dart
@@ -11,11 +11,11 @@ class Group {
 
   static const Group gt3 = const Group(
     name: 'GT3',
-    color: Color.fromARGB(255, 25, 25, 25),
+    color: Color.fromARGB(255, 194, 14, 24),
   );
   static const Group gt4 = const Group(
     name: 'GT4',
-    color: Color.fromARGB(255, 38, 38, 96),
+    color: Color.fromARGB(255, 42, 42, 100),
   );
   static const Group gtc = const Group(
     name: 'GTC',
